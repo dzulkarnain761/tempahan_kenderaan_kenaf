a8634f00fc0bc33272e3d2e06de3536044e7c192
nurulatikah03 <atikah9w2ser@gmail.com>
2024-09-04 11:09:29 +0800

    kppDashboard (edit title)

--- kppDashboard/kemaskiniProfile.php
+++ kppDashboard/kemaskiniProfile.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Booking</title>
+	<title>eBooking</title>
+	<link rel="icon" type="image/x-icon" href="images/logo2.png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- kppDashboard/pengesahanTolakTerima.php
+++ kppDashboard/pengesahanTolakTerima.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Booking</title>
-    <!-- ======= Styles ====== -->
+	<title>eBooking</title>
+	<link rel="icon" type="image/x-icon" href="images/logo2.png">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
