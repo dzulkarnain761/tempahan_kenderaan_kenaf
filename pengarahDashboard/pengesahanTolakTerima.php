37866a56cc3f98e1a0ef6e70e0e7b54754df207c
nurulatikah03 <atikah9w2ser@gmail.com>
2024-09-12 15:15:07 +0800

    buang ii

--- pengarahDashboard/pengesahanTolakTerima.php
+++ pengarahDashboard/pengesahanTolakTerima.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>eBooking</title>
 	<link rel="icon" type="image/x-icon" href="images/logo2.png">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
